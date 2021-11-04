654b5273e89a9653fd946fc0a8577dd24e86c008
AmethystBird <sebastian.outram@outlook.com>
2021-11-04 19:46:34 +0000

    All screens created
    - Polished appearance of login screen
    - Implemented all other screen designs to varying degrees

---
+++ app/src/main/java/com/example/playermanagementsystem/NewScore.java
@@ -0,0 +1,14 @@
+package com.example.playermanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_score);
+    }
+}
