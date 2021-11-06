5b342687eada4bdcf201adea25c0b7f95d7b29fd
Nikita The Wyvern <48954549+AmethystBird@users.noreply.github.com>
2021-11-06 15:28:12 +0000

    Polished screens
    - Particularly developed score screen
    - Developing button functionality

--- app/src/main/java/com/example/playermanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/playermanagementsystem/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.playermanagementsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button testButton = findViewById(R.id.loginButton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+            }
+        });
     }
 }
