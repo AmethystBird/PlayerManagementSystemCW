7358524b84b088b3f57b2d3aa9020fc2ff773dfc
AmethystBird <sebastian.outram@outlook.com>
2021-11-08 20:04:14 +0000

    Field prompts on login screen added
    - Login screen fields must not be blank in order to use the login button
    - Fields are wiped when the user moves to another activity for cleanliness
    - Prompts will appear beneath fields requiring entry if a login is attempted regardless

--- app/src/main/java/com/example/playermanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/playermanagementsystem/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +22,58 @@ public class MainActivity extends AppCompatActivity {
     {
         if (view.getId() == R.id.loginButton) //Login button clicked
         {
-            Intent activityTransition = new Intent(MainActivity.this, Scores.class);
-            startActivity(activityTransition);
+            TextView inputUsernameOrEmailLabel = findViewById(R.id.inputUsernameOrEmailLabel);
+            TextView emailOrUsernamePrompt = findViewById(R.id.emailOrUsernamePrompt);
+
+            TextView inputPasswordLabel = findViewById(R.id.inputPasswordLabel);
+            TextView passwordPrompt = findViewById(R.id.passwordPrompt);
+
+            boolean missingFields = false;
+            if (inputUsernameOrEmailLabel.getText().toString().isEmpty()) //If the user has left the username or email TextView blank
+            {
+                emailOrUsernamePrompt.setText("Username or Email required.");
+                missingFields = true;
+            }
+            else if (!emailOrUsernamePrompt.getText().toString().isEmpty()) //If the user has left the username or email TextView blank
+            {
+                emailOrUsernamePrompt.setText("");
+            }
+            if (inputPasswordLabel.getText().toString().isEmpty()) //If the user has left the password TextView blank
+            {
+                passwordPrompt.setText("Password is required.");
+                missingFields = true;
+            }
+            else if (!passwordPrompt.getText().toString().isEmpty()) //If the user has left the username or email TextView blank
+            {
+                passwordPrompt.setText("");
+            }
+
+            if (missingFields == false) //If they have no missing fields
+            {
+                Intent activityTransition = new Intent(MainActivity.this, Scores.class);
+                startActivity(activityTransition);
+            }
         }
         else if (view.getId() == R.id.registerOptionButton) //Register option button clicked
         {
+            ResetActivity();
             Intent activityTransition = new Intent(MainActivity.this, Register.class);
             startActivity(activityTransition);
         }
+    }
+    
+    public void ResetActivity()
+    {
+        //Clearing prompts
+        TextView inputUsernameOrEmailLabel = findViewById(R.id.inputUsernameOrEmailLabel);
+        TextView emailOrUsernamePrompt = findViewById(R.id.emailOrUsernamePrompt);
+        TextView inputPasswordLabel = findViewById(R.id.inputPasswordLabel);
+        TextView passwordPrompt = findViewById(R.id.passwordPrompt);
+
+        inputUsernameOrEmailLabel.setText("");
+        emailOrUsernamePrompt.setText("");
+        inputPasswordLabel.setText("");
+        passwordPrompt.setText("");
     }
 }
 
